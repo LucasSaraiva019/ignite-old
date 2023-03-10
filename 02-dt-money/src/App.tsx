3d8a8618cf6a1a35c211c7eedff3ab782cc66b68
Lucas Saraiva <lucas.saraiva@hp.com>
2023-03-10 18:07:15 -0300

    adding hooks in the project

--- 02-dt-money/src/App.tsx
+++ 02-dt-money/src/App.tsx
@@ -3,9 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-
-
-
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
@@ -18,11 +16,11 @@ export function App() {
   }
 
   return (
-    <>
+   <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
