0a9747893562d1085c670aae64c95ccf7728c7e2
Lucas Saraiva <64505737+LucasSaraiva019@users.noreply.github.com>
2023-04-15 20:40:18 -0300

    adding final project

--- 04-dashgo/src/components/Form/Input.tsx
+++ 04-dashgo/src/components/Form/Input.tsx
@@ -1,25 +1,41 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { FieldError } from 'react-hook-form'
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps, FormErrorMessage } from '@chakra-ui/react'
 
 interface InputProps extends ChakraInputProps {
-  name: string
-  label?: string
+  name: string;
+  label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput
-        id={name}
-        name={name}
-        type='email'
-        focusBorderColor='pink.500'
-        bgColor="gray.900"
-        variant='filled'
-        _hover={{ bgColor: 'gray.900' }}
-        size='lg'
-        {...rest}
-      />
-    </FormControl>
-  )
-}
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, error = null, ...rest }, ref) => {
+    return (
+      <FormControl isInvalid={!!error}>
+        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+        <ChakraInput
+          id={name}
+          name={name}
+          ref={ref}
+          focusBorderColor='pink.500'
+          bgColor='gray.900'
+          variant='filled'
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          size='lg'
+          {...rest}
+        />
+
+        {!!error && (
+          <FormErrorMessage>
+            {error.message}
+          </FormErrorMessage>
+        )}
+      </FormControl>
+    )
+  }
+
+export const Input = forwardRef(InputBase)
--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -1,43 +1,114 @@
-import { Input } from "@/src/components/Form/Input";
-import { Header } from "@/src/components/Header";
-import { Sidebar } from "@/src/components/Sidebar";
-import { Box, Button, Divider, Flex, HStack, Heading, SimpleGrid, VStack } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+import { Input } from '../../components/Form/Input';
+
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([
+    undefined, yup.ref('password')
+  ], 'As senhas precisam ser iguais')
+})
 
 export default function CreateUser() {
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<CreateUserFormData>({
+    resolver: yupResolver(createUserFormSchema)
+  })
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+    // await createUser.mutateAsync(values)
+
+    // router.push('/users')
+  }
+
   return (
     <Box>
       <Header />
-
-      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
-
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
-          <Divider my="6" borderColor="gray.700" />
-
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
-              <Input name="name" label="Nome Completo" />
-              <Input name="email" type="email" label="E-mail" />
+        <Box
+          as='form'
+          flex='1'
+          borderRadius={8}
+          bg='gray.800'
+          p={['6', '8']}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
+          <Heading size='lg' fontWeight='normal'>Criar usuário</Heading>
+          <Divider my='6' borderColor='gray.700' />
+          <VStack spacing={['6', '8']}>
+            <SimpleGrid
+              minChildWidth='240px'
+              spacing={['6', '8']}
+              w='100%'
+            >
+              <Input
+                label='Nome completo'
+                type='text'
+                error={errors.name}
+                {...register('name')}
+              />
+              <Input
+                label='E-mail'
+                type='email'
+                error={errors.email}
+                {...register('email')}
+              />
             </SimpleGrid>
-
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+            <SimpleGrid
+              minChildWidth='240px'
+              spacing={['6', '8']}
+              w='100%'
+            >
+              <Input
+                label='Senha'
+                type='password'
+                error={errors.password}
+                {...register('password')}
+              />
+              <Input
+                label='Confirmação da senha'
+                type='password'
+                error={errors.password_confirmation}
+                {...register('password_confirmation')}
+              />
             </SimpleGrid>
           </VStack>
-
-          <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
+          <Flex mt='8' justify='flex-end'>
+            <HStack spacing='4'>
               <Link href='/users' passHref>
                 <Button as='a' colorScheme='whiteAlpha'>Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button
+                type='submit'
+                colorScheme='pink'
+                isLoading={isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
